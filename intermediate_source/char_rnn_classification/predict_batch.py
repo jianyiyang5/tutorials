96011349d6f5a0ea8f22b249916b3de02ce28dca
jianyi_yang <jianyi_yang@apple.com>
2020-03-19 23:28:11 -0700

    batch predict

---
+++ intermediate_source/char_rnn_classification/predict_batch.py
@@ -0,0 +1,26 @@
+from train_batch import *
+
+
+def predict(model, all_categories, input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        line_tensor, lens = linesToTensor([input_line])
+        output = evaluate(model, line_tensor, lens)
+        output = output.squeeze(dim=1)
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+
+if __name__ == '__main__':
+    model = load_model('output/rnn_batch.pt')
+    model.eval()
+    category_lines, all_categories = load_data()
+    names = ['Dovesky', 'Jackson', 'Satoshi', 'Chan', 'Jonas', 'Joris']
+    for name in names:
+        predict(model, all_categories, name)
--- intermediate_source/char_rnn_classification/train_batch.py
+++ intermediate_source/char_rnn_classification/train_batch.py
@@ -1,6 +1,3 @@
-#TODO batch train
-#x2 = x.transpose(0, 1)
-#y = x2[torch.arange(x2.size(0)), idx]
 import torch.nn as nn
 from torch.nn.modules.rnn import RNN
 from data import *
@@ -47,42 +44,18 @@ def train(rnn, category_lines, all_categories, batch_size=64, criterion=nn.NLLLo
             current_loss += loss.item()
             for p in rnn.parameters():
                 p.data.add_(-learning_rate, p.grad.data/lengths.size(0))
-        print(f'epoch {i} {round(current_loss/j, 4)}')
+        guess, guess_i = categoryFromOutput(output[0], all_categories)
+        correct = '✓' if guess == batch[0][1] else '✗ (%s)' % batch[0][1]
+        print('%d %d%% (%s) %.4f %s / %s %s' % (i, i / epochs * 100, timeSince(start), loss, batch[0], guess, correct))
         all_losses.append(current_loss/j)
         current_loss = 0
-            # Print iter number, loss, name and guess
-            # if iter % print_every == 0:
-            #     guess, guess_i = categoryFromOutput(outputs[0], all_categories)
-            #     correct = '✓' if guess == batches[0][1] else '✗ (%s)' % batches[0][1]
-            #     print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
-            # # Add current loss avg to list of losses
-            # if iter % plot_every == 0:
-            #     all_losses.append(current_loss / plot_every)
-            #     current_loss = 0
     return all_losses
 
 
 # Just return an output given a line
-def evaluate(rnn, line_tensor):
-    #hidden = rnn.initHidden()
-    for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
+def evaluate(rnn, line_tensor, lens):
+    output, hidden = rnn(line_tensor, lens)
     return output
-
-
-def create_confusion_matrix(rnn, category_lines, all_categories, n_confusion=10000):
-    n_categories = len(all_categories)
-    confusion = torch.zeros(n_categories, n_categories)
-    for i in range(n_confusion):
-        category, line, category_tensor, line_tensor = randomTrainingExample(category_lines, all_categories)
-        output = evaluate(rnn, line_tensor)
-        guess, guess_i = categoryFromOutput(output, all_categories)
-        category_i = all_categories.index(category)
-        confusion[category_i][guess_i] += 1
-    # Normalize by dividing every row by its sum
-    for i in range(n_categories):
-        confusion[i] = confusion[i] / confusion[i].sum()
-    return confusion
 
 
 def save_model(input_size, hidden_size, output_size, model_dict, path):
@@ -95,7 +68,10 @@ def save_model(input_size, hidden_size, output_size, model_dict, path):
 
 def load_model(path):
     checkpoint = torch.load(path)
-    model = RNN(checkpoint['input_size'], checkpoint['hidden_size'], checkpoint['output_size'])
+    n_hidden = checkpoint['hidden_size']
+    n_categories = checkpoint['output_size']
+    n_letters = checkpoint['input_size']
+    model = EncoderRNN(n_hidden, nn.Embedding(n_letters, n_hidden), n_categories)
     model.load_state_dict(checkpoint['model_dict'])
     print(f'Load model from {path}')
     return model
@@ -111,7 +87,4 @@ if __name__ == '__main__':
     rnn = EncoderRNN(n_hidden, nn.Embedding(n_letters, n_hidden), n_categories)
     all_losses = train(rnn, category_lines, all_categories)
     save_model(n_letters, n_hidden, n_categories, rnn.state_dict(), model_path)
-    # print(all_losses)
-    # plot_losses(all_losses)
-    # confusion = create_confusion_matrix(rnn, category_lines, all_categories)
-    # plot_confusion_matrix(confusion, all_categories)
+    plot_losses(all_losses)
