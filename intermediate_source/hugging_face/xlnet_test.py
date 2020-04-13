632a878d6230a21cb96435fda25f1d47f379b7b2
jianyi_yang <jianyi_yang@apple.com>
2020-04-12 22:36:25 -0700

    gpt

--- intermediate_source/hugging_face/xlnet_test.py
+++ intermediate_source/hugging_face/xlnet_test.py
@@ -11,6 +11,27 @@ last_hidden_states = outputs[0]  # The last hidden-state is the first element of
 print(last_hidden_states[0].size())
 
 
+# from transformers import XLNetTokenizer, XLNetLMHeadModel
+# import torch
+#
+# tokenizer = XLNetTokenizer.from_pretrained('xlnet-large-cased')
+# model = XLNetLMHeadModel.from_pretrained('xlnet-large-cased')
+
+# # We show how to setup inputs to predict a next token using a bi-directional context.
+# input_ids = torch.tensor(tokenizer.encode("Hello, my dog is very <mask>", add_special_tokens=False)).unsqueeze(0)  # We will predict the masked token
+# perm_mask = torch.zeros((1, input_ids.shape[1], input_ids.shape[1]), dtype=torch.float)
+# perm_mask[:, :, -1] = 1.0  # Previous tokens don't see last token
+# target_mapping = torch.zeros((1, 1, input_ids.shape[1]), dtype=torch.float)  # Shape [1, 1, seq_length] => let's predict one token
+# target_mapping[0, 0, -1] = 1.0  # Our first (and only) prediction will be the last token of the sequence (the masked token)
+#
+# outputs = model(input_ids, perm_mask=perm_mask, target_mapping=target_mapping)
+# next_token_logits = outputs[0]  # Output has shape [target_mapping.size(0), target_mapping.size(1), config.vocab_size]
+# print(next_token_logits.size())
+# predicted_index = torch.argmax(next_token_logits[0, -1, :])
+# print(predicted_index)
+# print(tokenizer.decode([predicted_index.item()]))
+
+
 from transformers import XLNetTokenizer, XLNetLMHeadModel
 import torch
 
@@ -26,7 +47,6 @@ target_mapping[0, 0, -1] = 1.0  # Our first (and only) prediction will be the la
 
 outputs = model(input_ids, perm_mask=perm_mask, target_mapping=target_mapping)
 next_token_logits = outputs[0]  # Output has shape [target_mapping.size(0), target_mapping.size(1), config.vocab_size]
-print(next_token_logits.size())
 
 # The same way can the XLNetLMHeadModel be used to be trained by standard auto-regressive language modeling.
 input_ids = torch.tensor(tokenizer.encode("Hello, my dog is very <mask>", add_special_tokens=False)).unsqueeze(0)  # We will predict the masked token
@@ -40,3 +60,6 @@ target_mapping[0, 0, -1] = 1.0  # Our first (and only) prediction will be the la
 outputs = model(input_ids, perm_mask=perm_mask, target_mapping=target_mapping, labels=labels)
 loss, next_token_logits = outputs[:2]  # Output has shape [target_mapping.size(0), target_mapping.size(1), config.vocab_size]
 print(next_token_logits.size())
+predicted_index = torch.argmax(next_token_logits[0, -1, :])
+print(predicted_index)
+print(tokenizer.decode([predicted_index.item()]))
